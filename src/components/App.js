e480ee27d6c859cd57606cdd897c3ad98a864a48
Evgeny Zhdanov <evgeny1halvin@yandex.ru>
2021-10-01 06:13:26 +0200

    added css and html

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
 import react, { useState, useEffect } from "react";
+import { Route, Switch, useHistory } from "react-router-dom";
+
+import Login from "./Login";
 
 function App() {
-  return (
-    <></>
-  );
+  return <div className="page">
+    <Switch>
+      <Route path="/sign-in">
+        <Login />
+      </Route>
+
+      <ProtectedRoute
+        path="/"
+      />
+    </Switch>
+  </div>
 }
 
 export default App;
