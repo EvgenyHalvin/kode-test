c7621d09a598e600888f6796432fc0597c72c102
Evgeny Zhdanov <evgeny1halvin@yandex.ru>
2022-01-04 07:21:29 +0200

    chore: добавил комментарий

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -17,6 +17,7 @@ function Sidebar(props) {
   const history = useHistory();
   const { search } = useLocation();
 
+  // Преобразование строки в объект
   const deserializeQuery = (query) => {
     const pairs = query.substring(1).split('&');
     const array = pairs.map((elem) => elem.split('='));
